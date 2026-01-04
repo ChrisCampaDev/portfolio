9be0b40672f9037380a69a870dde972528f9644d
ChrisCampaDev <christiancampa546@gmail.com>
2026-01-04 18:27:10 -0500

    fix

--- src/store/store.ts
+++
@@ -1,3 +0,0 @@
-import { atom } from "nanostores";
-
-export const message = atom("");
---
+++ src/utils/const.ts
@@ -0,0 +1,4 @@
+export const telf = "54758835";
+export const mail = "christiancampa546@gmail.com";
+const ApiFormSubmit =
+	"1ce31ff3912652ded766b3cf5c25287c108661bfc20f8b99475e47ef88b99699";
